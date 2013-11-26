c8ec3d04a09538767a8e60feeaaf2fc8d187c110
azubear <larisa.purda@gmail.com>
2013-11-26 18:39:30 +0200

    printul

---
+++ src/com/azubear/wackyfunjava/L1.java
@@ -0,0 +1,10 @@
+package com.azubear.wackyfunjava;
+
+public class L1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Print print print!");
+	}
+
+}
